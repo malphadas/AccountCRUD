f8799ab3a40eb6fe7175f71ae82a4b809f42bcd3
Matheus Alpha de Andrade Souza <matheus.alphadas@gmail.com>
2023-06-17 17:59:25 -0300

    Implemented JWT authorization

--- acccrud/src/main/java/com/malpha/acccrud/Model/Usuario.java
+++ acccrud/src/main/java/com/malpha/acccrud/Model/Usuario.java
@@ -9,6 +9,13 @@ import jakarta.persistence.Id;
 @Entity
 public class Usuario {
 
+    public Usuario(long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -21,5 +28,37 @@ public class Usuario {
     
     @Column(nullable = false)
     private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
 }
