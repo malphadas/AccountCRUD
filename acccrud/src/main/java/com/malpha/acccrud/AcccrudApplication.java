c01e713e5ca576f1b8f03b3a27897012c920874c
Matheus Alpha de Andrade Souza <matheus.alphadas@gmail.com>
2023-06-17 15:29:08 -0300

    Initial commit

---
+++ acccrud/src/main/java/com/malpha/acccrud/AcccrudApplication.java
@@ -0,0 +1,13 @@
+package com.malpha.acccrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcccrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcccrudApplication.class, args);
+	}
+
+}
---
+++ acccrud/src/test/java/com/malpha/acccrud/AcccrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malpha.acccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcccrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
