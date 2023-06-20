1e9de74674f14a0b05d4bd9f2bae073853cc33a5
Matheus Alpha de Andrade Souza <matheus.alphadas@gmail.com>
2023-06-20 18:54:20 -0300

    Webpages

--- acccrud/src/main/java/com/malpha/acccrud/Controller/HomeController.java
+++ acccrud/src/main/java/com/malpha/acccrud/Controller/HomeController.java
@@ -1,14 +1,15 @@
 package com.malpha.acccrud.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     
     @GetMapping("/")
     public String home(){
-        return "Hello, User!";
+        return "home.html";
     }
 
     @GetMapping("/locked")
